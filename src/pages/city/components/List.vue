8e4ed77ba0d95da1f068a4cd39a69fea32b6c9ce
Your Name <you@example.com>
2019-11-09 16:46:35 +0800

    1

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,54 +1,50 @@
 <template>
-    <div class="list">
-        <div class="area">
-            <div class="title border-topbottom"> 当前城市</div>
-            <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+    <div class="list" ref="wrapper">
+        <div>
+            <div class="area">
+                <div class="title border-topbottom"> 当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">bj</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="area">
-            <div class="title border-topbottom"> 热门城市</div>
-            <div class="button-list">
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
-                </div>
-                <div class="button-wrapper">
-                    <div class="button">北京</div>
+            <div class="area">
+                <div class="title border-topbottom"> 热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper"
+                    v-for="item of hot"
+                    :key="item.id">
+                        <div class="button">{{item.name}}</div>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="area">
-            <div class="title border-topbottom"> A</div>
+            <div class="area" v-for="(item,key) of cities" :key="key">
+                <div class="title border-topbottom">{{key}}</div>
+                <div class="item-list">
+                    <div class="item border-bottom"
+                         v-for="innerItem of item"
+                         :key="innerItem.id"
+                    >{{innerItem.name}}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.wrapper)
+  }
 }
 </script>
 
@@ -61,25 +57,42 @@ export default {
         &:after
             border-color: #ccc
 
-    .title
-        line-height: .44rem
-        background: #eee
-        padding-left: .2rem
-        color: #666
-        font-size: .26rem
+    .border-bottom
+    &:before
+        border-color: #ccc
 
-    .button-list
-        padding: .1rem .6rem .1rem .1rem
+    .list
         overflow: hidden
+        position: absolute
+        top: 1.6rem
+        left: 0
+        right: 0
+        bottom: 0
 
-        .button-wrapper
-            float: left
-            width: 33.33%
+        .title
+            line-height: .55rem
+            background: #eee
+            padding-left: .2rem
+            color: #666
+            font-size: .26rem
 
-            .button
-                margin: .1rem
-                padding: .1rem 0
-                text-align: center
-                border: .02rem solid #ccc
-                border-radius: .06rem
+        .button-list
+            padding: .1rem .6rem .1rem .1rem
+            overflow: hidden
+
+            .button-wrapper
+                float: left
+                width: 33.33%
+
+                .button
+                    margin: .1rem
+                    padding: .1rem 0
+                    text-align: center
+                    border: .02rem solid #ccc
+                    border-radius: .06rem
+
+        .item-list
+            .item
+                line-height: .76rem
+                padding-left: .2rem
 </style>
