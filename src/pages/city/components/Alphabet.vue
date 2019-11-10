09de4bd79861de3ed403dc1dc0ecbe04ca4ab1ba
Your Name <you@example.com>
2019-11-10 16:59:22 +0800

    1

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,8 +1,14 @@
 <template>
     <ul class="list">
         <li class="item"
-            v-for="(item,key) of cities"
-            :key="key">{{key}}</li>
+            v-for="item of letters"
+            :key="item"
+            :ref="item"
+            @click="handleLetterClick"
+            @touchstart="handleTouchStart"
+            @touchmove="handleTouchMove"
+            @touchend="handleTouchEnd">{{item}}
+        </li>
     </ul>
 </template>
 
@@ -11,6 +17,41 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
@@ -18,18 +59,18 @@ export default {
 <style lang="stylus" scoped>
     @import "~styles/varibles.styl"
     .list
-        position:absolute
-        top:1.58rem
-        right:0
-        bottom:0
-        width:.4rem
-        display:flex
-        flex-direction :column
-        justify-content:center
+        position: absolute
+        top: 1.58rem
+        right: 0
+        bottom: 0
+        width: .4rem
+        display: flex
+        flex-direction: column
+        justify-content: center
 
         .item
-            line-height:.4rem
-            text-align :center
-            color:$bgColor
+            line-height: .4rem
+            text-align: center
+            color: $bgColor
 
 </style>
--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -13,13 +13,16 @@
                 <div class="title border-topbottom"> 热门城市</div>
                 <div class="button-list">
                     <div class="button-wrapper"
-                    v-for="item of hot"
-                    :key="item.id">
+                         v-for="item of hot"
+                         :key="item.id">
                         <div class="button">{{item.name}}</div>
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item,key) of cities" :key="key">
+            <div class="area"
+                 v-for="(item,key) of cities"
+                 :key="key"
+                 :ref="key">
                 <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div class="item border-bottom"
@@ -40,10 +43,19 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
